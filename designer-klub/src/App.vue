dca0c2c73c5e701d9113440ddd54f52b8860ea03
gillet-thomas <thomas.gillet43@gmail.com>
2021-11-15 16:52:27 +0100

    init map

--- designer-klub/src/App.vue
+++ designer-klub/src/App.vue
@@ -1,11 +1,22 @@
 <template>
   <div id="app">
-    <router-view />
+    <Map
+      style="
+        width: 60%;
+        height: 30%;
+        margin-left: 400px;
+        -webkit-filter: grayscale(100%);
+        filter: grayscale(100%);
+      "
+    />
   </div>
 </template>
 <script>
+import Map from './components/Map';
 export default {
-  name: "App",
-  mounted() {},
+  name: 'App',
+  components: {
+    Map,
+  },
 };
 </script>
