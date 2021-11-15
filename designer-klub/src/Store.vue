972228c936cb3221a05cabcd6653850091bf3886
gillet-thomas <thomas.gillet43@gmail.com>
2021-11-15 16:43:25 +0100

    Init

---
+++ designer-klub/src/Store.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h1>{{ message }}</h1>
+    <button @click="increment">Increment</button>
+    <button @click="decrement">Decrement</button>
+    Store {{ id }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Store",
+  data() {
+    return {
+      message: "Hello Vue!",
+      id: null,
+    };
+  },
+  methods: {
+    increment() {
+      this.message += "!";
+    },
+    decrement() {
+      this.message = this.message.slice(0, -1);
+    },
+  },
+  mounted() {
+    console.log(this.$route.params.id);
+    this.id = this.$route.params.id;
+  },
+};
+</script>
+
+<style></style>
--- designer-klub/src/main.js
+++ designer-klub/src/main.js
@@ -1,8 +1,56 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+import { Row, Column } from "vue-grid-responsive";
+
+Vue.component("row", Row);
+Vue.component("column", Column);
+
+// import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
+
+// Import Bootstrap an BootstrapVue CSS files (order is important)
+// import "bootstrap/dist/css/bootstrap.css";
+// import "bootstrap-vue/dist/bootstrap-vue.css";
+
+// Make BootstrapVue available throughout your project
+// Vue.use(BootstrapVue);
+// Optionally install the BootstrapVue icon components plugin
+// Vue.use(IconsPlugin);
+
+import Vuetify from "vuetify";
+import "vuetify/dist/vuetify.min.css";
+
+Vue.use(Vuetify);
+
+import route from "vue-router";
+import home from "./Home.vue";
+import store from "./Store.vue";
+
+Vue.use(route);
+
+const router = new route({
+  mode: "history",
+  routes: [
+    {
+      path: "/",
+      name: "home",
+      component: home,
+    },
+    {
+      path: "/store",
+      name: "store",
+      component: store,
+    },
+    {
+      path: "*",
+      redirect: "/",
+    },
+  ],
+});
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  router,
+  render: (h) => h(App),
+}).$mount("#app");
